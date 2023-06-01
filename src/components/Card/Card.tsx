cef0d1a9c71023555e285a9b44e4b8ad136f87c3
AssFerj <assisjuniorcam@gmail.com>
2023-06-01 18:55:36 -0300

    clone master branch

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+interface MediaCardProps {
+    title: string
+    description: string
+    btnTitle: string
+}
+
+const MediaCard: React.FC<MediaCardProps> = ({title, description, btnTitle}) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">{btnTitle}</Button>
+        {/* <Button size="small">Learn More</Button> */}
+      </CardActions>
+    </Card>
+  );
+};
+
+export default MediaCard;
